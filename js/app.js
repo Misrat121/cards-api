eabdebf86cb85fba809e170a3a7cbd141d4af994
Misrat121 <you@example.com>
2022-02-28 11:23:01 +0600

    done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 //  2. get input value 
 //  3. error handling for string value
 
+const main = document.getElementById("main");
 const searchButton = () => {
     const input = document.getElementById("input-value");
     const error = document.getElementById("error");
@@ -12,24 +13,62 @@ const searchButton = () => {
         // alert("please enter a number");
         error.innerText = "please give a number";
         input.value = "";
+        main.innerHTML ="";
     }
     else if(inputValue <= 0){
         error.innerText = "please give a valid number";
         input.value = "";
+        main.innerHTML="";
     }
     else{
+        main.innerHTML="";
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
         .then(res => res.json())
         .then(data => cardsDisplay(data.cards))
         input.value="";
+        error.innerHTML="";
     }
 }
 const cardsDisplay = (cards) =>{
     // cards= cards.cards;
     console.log(cards);
-    
     for(const card of cards){
-       
+        console.log(card.code);
+       const div = document.createElement("div");
+       div.classList.add("col-lg-4")
+       div.classList.add("mb-5")
+       div.innerHTML = `
+       <div class="card" style="width: 18rem;">
+  <img src="${card.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${card.suit}</h5>
+    <p class="card-text"> ${card.code}</p>
+    <button onclick="cardDetails('${card.code}')" class="btn btn-primary"> See Details</button>
+  </div>
+</div>
+       `
+       main.appendChild(div)
     }
 
+}
+const cardDetails = (code) =>{
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+        .then(res => res.json())
+        .then(data => {
+          const allCards = data.cards;
+            const singleCard = allCards.find(card => card.code === code)
+           const div = document.createElement("div");
+           main.innerHTML="";
+           div.innerHTML=`
+           <div class="card" style="width: 18rem;">
+           <img src="${singleCard.image}" class="card-img-top" alt="...">
+           <div class="card-body">
+             <h5 class="card-title">${singleCard.suit}</h5>
+             <p class="card-text"> ${singleCard.code}</p>
+            
+           </div>
+         </div>
+           `
+           main.appendChild(div)
+        })
 }
